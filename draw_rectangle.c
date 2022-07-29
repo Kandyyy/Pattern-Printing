e6c3163a4e9d801f74dba91353253d0da09a607f
Kandyyy <78207452+Kandyyy@users.noreply.github.com>
2022-07-29 23:23:37 +0530

    Refactoring

--- draw_rectangle.c
+++ draw_rectangle.c
@@ -3,32 +3,17 @@
 int draw_rectangle(int length, int breadth){
 
 for(int i = 0; i <= breadth-1; i++){
-
-        
         for(int x = 0; x <= length-2; x++){
+                printf("*");
+        }
 
         printf("*");
-
+        printf("\n");
     }
-
-    printf("*");
-    printf("\n");
-
-    }
-
 }
 
-
-
-
 int main(){
-
-    
-
     draw_rectangle(15,10);
-
-    
-
     return 0;
 
 }
